c8433b14ba1378076c64c2bb30002057dfc22ba5
Dolphin <wdschn2016@gmail.com>
2022-04-10 19:33:05 +0800

    Add custom user

---
+++ user/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.apps import apps
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        # email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    # email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+
+    # EMAIL_FIELD = 'email'
+    # REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        app_label = 'user'
+        db_table = 'user'
+        verbose_name = _('user')
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
