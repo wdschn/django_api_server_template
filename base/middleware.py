f595235c6170096953db5b7c54df84d4eb8f607f
Dolphin <wdschn2016@gmail.com>
2022-04-14 00:38:40 +0800

    add process duration middleware

---
+++ base/middleware.py
@@ -0,0 +1,12 @@
+import time
+
+
+class ProcessDurationMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time.time()
+        response = self.get_response(request)
+        response['X-Page-Duration-ms'] = int((time.time() - start_time) * 1000)
+        return response
